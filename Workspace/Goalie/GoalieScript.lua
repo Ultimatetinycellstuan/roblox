7f0fd0390a734aa4a10c1c5c35b1ce21da4402db
Ultimatetinycellstuan <92477885+Ultimatetinycellstuan@users.noreply.github.com>
2022-08-09 10:10:15 -0500

    Create GoalieScript.lua

---
+++ Workspace/Goalie/GoalieScript.lua
@@ -0,0 +1,15 @@
+wall = script.Parent.Wall
+left = script.Parent.Wall
+right = script.Parent.Wall
+centralPosition = script.Parent.Wall
+
+while true do
+  wait(3)
+  wall.Position = left.Position
+  wait(3)
+  wall.Position = central.Position
+  wait(3)
+  wall.Position = right.Positon
+  wait(3)
+	wall.Position = centralPosition
+end
